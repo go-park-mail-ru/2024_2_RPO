dc09c4bcdafe0ed2f71496b1d0ef0fc2c9776167
DeDxYk594 <zhugeo@mail.ru>
2024-10-01 21:36:29 +0300

    Сделано покрытие тестами 66%

--- get_coverage_data.sh
+++ get_coverage_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # TODO сделать запуск тестов, определение тестового покрытия и генерацию html-ки с результатами анализа
 
+go build
 go test ./... -coverprofile=coverage.out
 go tool cover -func=coverage.out
 go tool cover -html=coverage.out -o coverage.html
