f92dbe02a420834a59a3052f51deba17c6b4d803
DeDxYk594 <zhugeo@mail.ru>
2024-10-30 21:19:22 +0300

    Начата работа над запросом

--- internal/pkg/board/repository/cards.go
+++ internal/pkg/board/repository/cards.go
@@ -1,14 +1,29 @@
 package repository
 
-import "RPO_back/internal/models"
+import (
+	"RPO_back/internal/models"
+	"context"
+	"errors"
+
+	"github.com/jackc/pgx/v5"
+)
 
 // GetCardsForBoard возвращает все карточки, размещённые на доске
-func (r *BoardRepository) GetCardsForBoard(boardId int) (cards []models.Card, err error) {
+func (r *BoardRepository) GetCardsForBoard(boardID int) (cards []models.Card, err error) {
+	query := `SELECT `
+	tag, err := r.db.Query(context.Background(), query)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, nil
+		}
+	}
 	panic("Not implemented")
 }
 
 // GetColumnsForBoard возвращает все колонки, которые есть на доске
-func (r *BoardRepository) GetColumnsForBoard(boardId int) (columns []models.Column, err error) {
+func (r *BoardRepository) GetColumnsForBoard(boardID int) (columns []models.Column, err error) {
+	query := ``
+	r.db.Query(context.Background(), query)
 	panic("Not implemented")
 }
 
@@ -17,16 +32,22 @@ func (r *BoardRepository) GetColumnsForBoard(boardId int) (columns []models.Colu
 // Что-то типа дополнительного уровня защиты
 
 // CreateNewCard создаёт новую карточку
-func (r *BoardRepository) CreateNewCard(boardId int, columnId int, title string) (newCard *models.Card, err error) {
+func (r *BoardRepository) CreateNewCard(boardID int, columnID int, title string) (newCard *models.Card, err error) {
+	query := ``
+	r.db.Query(context.Background(), query)
 	panic("Not implemented")
 }
 
 // UpdateCard обновляет карточку
-func (r *BoardRepository) UpdateCard(boardId int, cardId int, data models.CardPatchRequest) (newCard *models.Card, err error) {
+func (r *BoardRepository) UpdateCard(boardID int, cardID int, data models.CardPatchRequest) (newCard *models.Card, err error) {
+	query := ``
+	r.db.Query(context.Background(), query)
 	panic("Not implemented")
 }
 
 // DeleteCard удаляет карточку
-func (r *BoardRepository) DeleteCard(boardId int, cardId int) (err error) {
+func (r *BoardRepository) DeleteCard(boardID int, cardID int) (err error) {
+	query := ``
+	r.db.Query(context.Background(), query)
 	panic("Not implemented")
 }
