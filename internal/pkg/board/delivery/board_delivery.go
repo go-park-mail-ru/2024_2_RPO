8da62fbac2b3e38ad6b863599265bb9e31d96b53
Valentin <v_stremin@mail.ru>
2024-10-31 01:14:52 +0300

    Работа на delivery

--- internal/pkg/board/delivery/board_delivery.go
+++ internal/pkg/board/delivery/board_delivery.go
@@ -1,11 +1,15 @@
 package delivery
 
 import (
+	"RPO_back/internal/errs"
 	"RPO_back/internal/models"
 	"RPO_back/internal/pkg/board/usecase"
+	"RPO_back/internal/pkg/middleware/session"
 	"RPO_back/internal/pkg/utils/requests"
 	"RPO_back/internal/pkg/utils/responses"
+	"errors"
 	"net/http"
+	"strconv"
 )
 
 type BoardDelivery struct {
@@ -58,40 +62,90 @@ func (d *BoardDelivery) RemoveMember(w http.ResponseWriter, r *http.Request) {
 
 // GetBoardContent получает все карточки и колонки с доски, а также информацию о доске
 func (d *BoardDelivery) GetBoardContent(w http.ResponseWriter, r *http.Request) {
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
+
+	userIDInt, err := strconv.Atoi(userID)
+	if err != nil {
+		return
+	}
+
 	var getBoardContentRequest models.BoardContent
-	err := requests.GetRequestData(r, &getBoardContentRequest)
+	err = requests.GetRequestData(r, &getBoardContentRequest)
 	if err != nil {
 		responses.DoBadResponse(w, http.StatusBadRequest, "Invalid request")
 		return
 	}
+
+	content, err := d.boardUsecase.GetBoardContent(userIDInt, getBoardContentRequest.BoardInfo.Id)
+	if err != nil {
+		if errors.Is(err, errs.ErrNotPermitted) {
+			responses.DoBadResponse(w, 403, "No rights to act")
+			return
+		}
+		if errors.Is(err, errs.ErrNotFound) {
+			responses.DoBadResponse(w, 404, "No such element was found")
+			return
+		}
+		responses.DoBadResponse(w, 500, "Internal Server Error")
+	}
+
+	responses.DoEmptyOkResponce(w)
 }
 
 // CreateNewCard создаёт новую карточку и возвращает её
 func (d *BoardDelivery) CreateNewCard(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
 
 // UpdateCard обновляет карточку и возвращает обновлённую версию
 func (d *BoardDelivery) UpdateCard(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
 
 // DeleteCard удаляет карточку
 func (d *BoardDelivery) DeleteCard(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
 
 // CreateColumn создаёт колонку канбана на доске и возвращает её
 func (d *BoardDelivery) CreateColumn(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
 
 // UpdateColumn изменяет колонку и возвращает её обновлённую версию
 func (d *BoardDelivery) UpdateColumn(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
 
 // DeleteColumn удаляет колонку
 func (d *BoardDelivery) DeleteColumn(w http.ResponseWriter, r *http.Request) {
-	panic("Not implemented")
+	userID, hasUserID := session.UserIDFromContext(r.Context())
+	if hasUserID == false {
+		responses.DoBadResponse(w, http.StatusUnauthorized, "unathorized")
+		return
+	}
 }
