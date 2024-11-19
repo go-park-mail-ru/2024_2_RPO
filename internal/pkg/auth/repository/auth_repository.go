980318af3cbe0bdcb6dcc38823c3f541acd243cf
Valentin <v_stremin@mail.ru>
2024-11-19 19:22:30 +0300

    Добавлена функция GetUserPasswordHashForUser

--- internal/pkg/auth/repository/auth_repository.go
+++ internal/pkg/auth/repository/auth_repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"RPO_back/internal/errs"
-	"RPO_back/internal/models"
 	"RPO_back/internal/pkg/utils/logging"
 	"RPO_back/internal/pkg/utils/pgxiface"
 	"context"
@@ -27,13 +26,13 @@ func CreateAuthRepository(postgresDb pgxiface.PgxIface, redisDb *redis.Client) *
 }
 
 // Регистрирует сессионную куку в Redis
-func (repo *AuthRepository) RegisterSessionRedis(ctx context.Context, cookie string, userID int) error {
-	redisConn := repo.redisDb.Conn(repo.redisDb.Context())
+func (r *AuthRepository) RegisterSessionRedis(ctx context.Context, cookie string, userID int) error {
+	redisConn := r.redisDb.Conn(r.redisDb.Context())
 	defer redisConn.Close()
 
 	ttl := 7 * 24 * time.Hour
 
-	err := redisConn.Set(repo.redisDb.Context(), cookie, userID, ttl).Err()
+	err := redisConn.Set(r.redisDb.Context(), cookie, userID, ttl).Err()
 	logging.Debug(ctx, "RegisterSessionRedis query to redis has err: ", err)
 	if err != nil {
 		return fmt.Errorf("unable to set session in Redis: %v", err)
@@ -43,11 +42,11 @@ func (repo *AuthRepository) RegisterSessionRedis(ctx context.Context, cookie str
 }
 
 // KillSessionRedis удаляет сессию из Redis
-func (repo *AuthRepository) KillSessionRedis(ctx context.Context, sessionID string) error {
-	redisConn := repo.redisDb.Conn(repo.redisDb.Context())
+func (r *AuthRepository) KillSessionRedis(ctx context.Context, sessionID string) error {
+	redisConn := r.redisDb.Conn(r.redisDb.Context())
 	defer redisConn.Close()
 
-	err := redisConn.Del(repo.redisDb.Context(), sessionID).Err()
+	err := redisConn.Del(r.redisDb.Context(), sessionID).Err()
 	logging.Debug(ctx, "KillSessionRedis query to redis has err: ", err)
 	if err != nil {
 		return err
@@ -57,11 +56,11 @@ func (repo *AuthRepository) KillSessionRedis(ctx context.Context, sessionID stri
 }
 
 // RetrieveUserIdFromSessionId ходит в Redis и получает UserID (или не получает и даёт ошибку errs.ErrNotFound)
-func (repo *AuthRepository) RetrieveUserIDFromSession(ctx context.Context, sessionID string) (userID int, err error) {
-	redisConn := repo.redisDb.Conn(repo.redisDb.Context())
+func (r *AuthRepository) RetrieveUserIDFromSession(ctx context.Context, sessionID string) (userID int, err error) {
+	redisConn := r.redisDb.Conn(r.redisDb.Context())
 	defer redisConn.Close()
 
-	val, err := redisConn.Get(repo.redisDb.Context(), sessionID).Result()
+	val, err := redisConn.Get(r.redisDb.Context(), sessionID).Result()
 	logging.Debug(ctx, "RetrieveUserIDFromSession query to redis has err: ", err)
 	if err == redis.Nil {
 		return 0, fmt.Errorf("RetrieveUserIDFromSession(%v): %w", sessionID, errs.ErrNotFound)
@@ -77,41 +76,14 @@ func (repo *AuthRepository) RetrieveUserIDFromSession(ctx context.Context, sessi
 	return intVal, nil
 }
 
-// GetUserByID получает данные пользователя из базы по id
-func (repo *AuthRepository) GetUserPasswordHash(ctx context.Context, userID int) (passwordHash string, err error) {
-	query := `
-	SELECT u_id, nickname, email, description,
-	joined_at, updated_at, password_hash
-	FROM "user"
-	WHERE u_id=$1;`
-	user = &models.UserProfile{}
-	err = repo.db.QueryRow(ctx, query, userID).Scan(
-		&user.ID,
-		&user.Name,
-		&user.Email,
-		&user.Description,
-		&user.JoinedAt,
-		&user.UpdatedAt,
-		&user.PasswordHash,
-	)
-	logging.Debug(ctx, "GetUserByID query has err: ", err)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, errs.ErrWrongCredentials
-		}
-		return nil, fmt.Errorf("GetUserByID: %w", err)
-	}
-	return user, nil
-}
-
 // SetNewPasswordHash устанавливает пользователю новый хеш пароля
-func (repo *AuthRepository) SetNewPasswordHash(ctx context.Context, userID int, newPasswordHash string) error {
+func (r *AuthRepository) SetNewPasswordHash(ctx context.Context, userID int, newPasswordHash string) error {
 	query := `
 	UPDATE "user"
 	SET password_hash=$1
 	WHERE u_id=$2;
 	`
-	tag, err := repo.db.Exec(ctx, query, newPasswordHash, userID)
+	tag, err := r.db.Exec(ctx, query, newPasswordHash, userID)
 	logging.Debug(ctx, "SetNewPasswordHash query has err: ", err, " tag: ", tag)
 	if err != nil {
 		return fmt.Errorf("SetNewPasswordHash: %w", err)
@@ -120,4 +92,23 @@ func (repo *AuthRepository) SetNewPasswordHash(ctx context.Context, userID int, 
 		return fmt.Errorf("SetNewPasswordHash: No password change done")
 	}
 	return nil
+}
+
+func (r *AuthRepository) GetUserPasswordHashForUser(ctx context.Context, userID int) (passwordHash string, err error) {
+	query := `
+	SELECT password_hash
+	FROM "user"
+	WHERE u_id = $1
+	`
+
+	err = r.db.QueryRow(ctx, query, userID).Scan(&passwordHash)
+	logging.Debug(ctx, "GetUserPasswordHashForUser query has err: ", err)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return "", errs.ErrNotFound
+		}
+		return "", fmt.Errorf("GetUserPasswordHashForUser: %w", err)
+	}
+
+	return passwordHash, nil
 }
--- internal/pkg/auth/usecase/auth_uc.go
+++ internal/pkg/auth/usecase/auth_uc.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"RPO_back/internal/pkg/auth"
+	"RPO_back/internal/pkg/utils/encrypt"
 	"context"
 )
 
@@ -16,7 +17,11 @@ func CreateAuthUsecase(repo auth.AuthRepo) *AuthUsecase {
 }
 
 func (uc *AuthUsecase) CreateSession(ctx context.Context, userID int64, password string) (sessionID string, err error) {
+	user, err := uc.authRepo.GetUserByID(ctx, int(userID))
+	if err != nil {
 
+	}
+	ok := encrypt.CheckPassword(password, user.PasswordHash)
 }
 
 func (uc *AuthUsecase) CheckSession(ctx context.Context, sessionID string) (userID int64, err error) {
