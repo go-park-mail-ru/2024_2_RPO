6de7a3a6018619fbac7226fc2eac3a862ea46b21
Valentin <v_stremin@mail.ru>
2024-11-19 17:50:35 +0300

    auth_delivery

--- internal/pkg/auth/delivery/auth_delivery.go
+++ internal/pkg/auth/delivery/auth_delivery.go
@@ -1,144 +1,54 @@
 package delivery
 
 import (
-	"RPO_back/internal/errs"
-	"RPO_back/internal/models"
 	"RPO_back/internal/pkg/auth"
-	"RPO_back/internal/pkg/utils/requests"
-	"RPO_back/internal/pkg/utils/responses"
-	"errors"
-	"net/http"
-	"time"
-
-	log "github.com/sirupsen/logrus"
+	"RPO_back/internal/pkg/auth/delivery/grpc/gen"
+	"context"
 )
 
-type AuthDelivery struct {
+type AuthServer struct {
+	gen.AuthServer
 	authUsecase auth.AuthUsecase
 }
 
-func CreateAuthDelivery(uc auth.AuthUsecase) *AuthDelivery {
-	return &AuthDelivery{
+func CreateAuthServer(uc auth.AuthUsecase) *AuthServer {
+	return &AuthServer{
 		authUsecase: uc,
 	}
 }
 
-// LoginUser обеспечивает вход в сервис
-func (this *AuthDelivery) LoginUser(w http.ResponseWriter, r *http.Request) {
-	// Получить данные из запроса
-	var loginRequest models.LoginRequest
-	err := requests.GetRequestData(r, &loginRequest)
+func (d *AuthServer) CreateSession(ctx context.Context, request *gen.UserDataRequest) (*gen.Session, error) {
+	sessionID, err := d.authUsecase.CreateSession(ctx, request.UserID, request.Password)
 	if err != nil {
-		responses.DoBadResponse(w, http.StatusBadRequest, "Invalid request")
-		log.Warn("LoginUser (getting data): ", err)
-		return
+		return &gen.Session{Error: gen.Error_INVALID_CREDENTIALS}, err
 	}
 
-	sessionID, err := this.authUsecase.LoginUser(r.Context(), loginRequest.Email, loginRequest.Password)
-	if err != nil {
-		if errors.Is(err, errs.ErrWrongCredentials) {
-			responses.DoBadResponse(w, 401, "Wrong credentials")
-			log.Warn("LoginUser (checking credentials): ", err)
-			return
-		}
-		responses.DoBadResponse(w, 500, "Internal Server Error")
-		log.Error("LoginUser (checking credentials): ", err)
-		return
-	}
-
-	cookie := http.Cookie{
-		Name:     auth.SessionCookieName,
-		Value:    sessionID,
-		Path:     "/",
-		HttpOnly: true,
-		MaxAge:   int((7 * 24 * time.Hour).Seconds()),
-	}
-	http.SetCookie(w, &cookie)
-
-	responses.DoEmptyOkResponse(w)
+	return &gen.Session{SessionID: sessionID, Error: gen.Error_NONE}, nil
 }
 
-// RegisterUser регистрирует пользователя
-func (this *AuthDelivery) RegisterUser(w http.ResponseWriter, r *http.Request) {
-	var user models.UserRegisterRequest
-	err := requests.GetRequestData(r, &user)
+func (d *AuthServer) CheckSession(ctx context.Context, request *gen.Session) (*gen.UserDataResponse, error) {
+	userID, err := d.authUsecase.CheckSession(ctx, request.SessionID)
 	if err != nil {
-		log.Error("AuthDelivery Parsing JSON: ", err)
-		responses.DoBadResponse(w, http.StatusBadRequest, "Bad request")
-		return
+		return &gen.UserDataResponse{Error: gen.Error_INVALID_CREDENTIALS}, err
 	}
 
-	sessionID, err := this.authUsecase.RegisterUser(r.Context(), &user)
-	if err != nil {
-		log.Error("Auth: ", err)
-		if errors.Is(err, errs.ErrBusyEmail) && errors.Is(err, errs.ErrBusyNickname) {
-			responses.DoBadResponse(w, http.StatusConflict, "Email and nickname are busy")
-		} else if errors.Is(err, errs.ErrBusyEmail) {
-			responses.DoBadResponse(w, http.StatusConflict, "Email is busy")
-		} else if errors.Is(err, errs.ErrBusyNickname) {
-			responses.DoBadResponse(w, http.StatusConflict, "Nickname is busy")
-		} else {
-			responses.DoBadResponse(w, http.StatusInternalServerError, "Internal server error")
-		}
-		return
-	}
-
-	cookie := http.Cookie{
-		Name:     auth.SessionCookieName,
-		Value:    sessionID,
-		SameSite: http.SameSiteStrictMode,
-		Path:     "/",
-		HttpOnly: true,
-		MaxAge:   int((7 * 24 * time.Hour).Seconds()),
-	}
-	http.SetCookie(w, &cookie)
-
-	responses.DoEmptyOkResponse(w)
+	return &gen.UserDataResponse{UserID: userID, Error: gen.Error_NONE}, nil
 }
 
-// LogoutUser разлогинивает пользователя
-func (this *AuthDelivery) LogoutUser(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie(auth.SessionCookieName)
+func (d *AuthServer) DeleteSession(ctx context.Context, request *gen.Session) (*gen.StatusResponse, error) {
+	err := d.authUsecase.KillSession(ctx, request.SessionID)
 	if err != nil {
-		responses.DoBadResponse(w, http.StatusBadRequest, "You are not logged in")
-		return
+		return &gen.StatusResponse{Error: gen.Error_INTERNAL_SERVER_ERROR}, nil
 	}
 
-	sessionID := cookie.Value
-
-	err = this.authUsecase.LogoutUser(r.Context(), sessionID)
-
-	http.SetCookie(w, &http.Cookie{
-		Name:   auth.SessionCookieName,
-		Value:  "",
-		Path:   "/",
-		MaxAge: -1,
-	})
-
-	if err != nil {
-		responses.ResponseErrorAndLog(w, err, "LogoutUser")
-	} else {
-		responses.DoEmptyOkResponse(w)
-	}
+	return &gen.StatusResponse{Error: gen.Error_NONE}, nil
 }
 
-// ChangePassword отвечает за смену пароля
-func (d *AuthDelivery) ChangePassword(w http.ResponseWriter, r *http.Request) {
-	funcName := "ChangePassword"
-	userID, ok := requests.GetUserIDOrFail(w, r, funcName)
-	if !ok {
-		return
-	}
-	data := models.ChangePasswordRequest{}
-	err := requests.GetRequestData(r, &data)
+func (d *AuthServer) ChangePassword(ctx context.Context, request *gen.ChangePasswordRequest) (*gen.StatusResponse, error) {
+	err := d.authUsecase.ChangePassword(ctx, request.PasswordOld, request.PasswordNew)
 	if err != nil {
-		responses.DoBadResponse(w, http.StatusBadRequest, "bad request")
-		return
+		return &gen.StatusResponse{Error: gen.Error_INTERNAL_SERVER_ERROR}, nil
 	}
-	err = d.authUsecase.ChangePassword(r.Context(), userID, data.OldPassword, data.NewPassword)
-	if err != nil {
-		responses.DoBadResponse(w, http.StatusInternalServerError, "internal error")
-		return
-	}
-	responses.DoEmptyOkResponse(w)
+
+	return &gen.StatusResponse{Error: gen.Error_NONE}, nil
 }
--- internal/pkg/auth/usecase/auth_uc.go
+++ internal/pkg/auth/usecase/auth_uc.go
@@ -1,15 +1,8 @@
 package usecase
 
 import (
-	"RPO_back/internal/errs"
-	"RPO_back/internal/models"
 	"RPO_back/internal/pkg/auth"
-	"RPO_back/internal/pkg/utils/encrypt"
 	"context"
-	"errors"
-	"fmt"
-
-	log "github.com/sirupsen/logrus"
 )
 
 type AuthUsecase struct {
@@ -22,70 +15,18 @@ func CreateAuthUsecase(repo auth.AuthRepo) *AuthUsecase {
 	}
 }
 
-func (uc *AuthUsecase) LoginUser(ctx context.Context, email string, password string) (sessionID string, err error) {
-	user, err := uc.authRepo.GetUserByEmail(ctx, email)
-	if err != nil {
-		return "", err
-	}
+func (uc *AuthUsecase) CreateSession(ctx context.Context, userID int64, password string) (sessionID string, err error) {
 
-	ok := encrypt.CheckPassword(password, user.PasswordHash)
-	if !ok {
-		return "", fmt.Errorf("LoginUser: passwords not match: %w", errs.ErrWrongCredentials)
-	}
-
-	sessionID = encrypt.GenerateSessionID()
-	err = uc.authRepo.RegisterSessionRedis(ctx, sessionID, user.ID)
-	if err != nil {
-		return "", err
-	}
-
-	return sessionID, nil
 }
 
-func (uc *AuthUsecase) RegisterUser(ctx context.Context, user *models.UserRegisterRequest) (sessionID string, err error) {
-	err = uc.authRepo.CheckUniqueCredentials(ctx, user.Name, user.Email)
-	if err != nil {
-		return "", err
-	}
+func (uc *AuthUsecase) CheckSession(ctx context.Context, sessionID string) (userID int64, err error) {
 
-	hashedPassword, err := encrypt.SaltAndHashPassword(user.Password)
-	if err != nil {
-		return "", errors.New("failed to hash password")
-	}
-
-	log.Info("Password hash: ", hashedPassword)
-
-	newUser, err := uc.authRepo.CreateUser(ctx, user, string(hashedPassword))
-	if err != nil {
-		return "", fmt.Errorf("internal error: %w", err)
-	}
-
-	sessionID = encrypt.GenerateSessionID()
-	err = uc.authRepo.RegisterSessionRedis(ctx, sessionID, newUser.ID)
-	if err != nil {
-		return "", errors.New("failed to register session")
-	}
-
-	return sessionID, nil
 }
 
-func (uc *AuthUsecase) LogoutUser(ctx context.Context, sessionID string) error {
-	return uc.authRepo.KillSessionRedis(ctx, sessionID)
+func (uc *AuthUsecase) KillSession(ctx context.Context, sessionID string) (err error) {
+
 }
 
-func (uc *AuthUsecase) ChangePassword(ctx context.Context, userID int, oldPassword string, newPassword string) error {
-	user, err := uc.authRepo.GetUserByID(ctx, userID)
-	if err != nil {
-		return err
-	}
-	ok := encrypt.CheckPassword(oldPassword, user.PasswordHash)
-	if !ok {
-		return fmt.Errorf("ChangePassword: %w", errs.ErrNotPermitted)
-	}
-	newPasswordHash, err := encrypt.SaltAndHashPassword(newPassword)
-	if err != nil {
-		return fmt.Errorf("ChangePassword (hashing new): %w", err)
-	}
-	uc.authRepo.SetNewPasswordHash(ctx, userID, newPasswordHash)
-	return nil
+func (uc *AuthUsecase) ChangePassword(ctx context.Context, oldPassword string, newPassword string) (err error) {
+
 }
