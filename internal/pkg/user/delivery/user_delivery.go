0b243b126c77c9187ae0d9cc08f82ca043be05ea
Valentin <v_stremin@mail.ru>
2024-11-19 17:16:24 +0300

    Перенес auth delivery в user

--- internal/pkg/user/delivery/user_delivery.go
+++ internal/pkg/user/delivery/user_delivery.go
@@ -1,11 +1,15 @@
 package delivery
 
 import (
+	"RPO_back/internal/errs"
 	"RPO_back/internal/models"
+	"RPO_back/internal/pkg/auth"
 	"RPO_back/internal/pkg/user"
 	"RPO_back/internal/pkg/utils/requests"
 	"RPO_back/internal/pkg/utils/responses"
+	"errors"
 	"net/http"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -81,4 +85,124 @@ func (d *UserDelivery) SetMyAvatar(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responses.DoJSONResponse(w, updatedProfile, 200)
+}
+
+// LoginUser обеспечивает вход в сервис
+func (d *UserDelivery) LoginUser(w http.ResponseWriter, r *http.Request) {
+	// Получить данные из запроса
+	var loginRequest models.LoginRequest
+	err := requests.GetRequestData(r, &loginRequest)
+	if err != nil {
+		responses.DoBadResponse(w, http.StatusBadRequest, "Invalid request")
+		log.Warn("LoginUser (getting data): ", err)
+		return
+	}
+
+	sessionID, err := d.userUC.LoginUser(r.Context(), loginRequest.Email, loginRequest.Password)
+	if err != nil {
+		if errors.Is(err, errs.ErrWrongCredentials) {
+			responses.DoBadResponse(w, 401, "Wrong credentials")
+			log.Warn("LoginUser (checking credentials): ", err)
+			return
+		}
+		responses.DoBadResponse(w, 500, "Internal Server Error")
+		log.Error("LoginUser (checking credentials): ", err)
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:     auth.SessionCookieName,
+		Value:    sessionID,
+		Path:     "/",
+		HttpOnly: true,
+		MaxAge:   int((7 * 24 * time.Hour).Seconds()),
+	}
+	http.SetCookie(w, &cookie)
+
+	responses.DoEmptyOkResponse(w)
+}
+
+// RegisterUser регистрирует пользователя
+func (d *UserDelivery) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user models.UserRegistration
+	err := requests.GetRequestData(r, &user)
+	if err != nil {
+		log.Error("UserDelivery Parsing JSON: ", err)
+		responses.DoBadResponse(w, http.StatusBadRequest, "Bad request")
+		return
+	}
+
+	sessionID, err := d.userUC.RegisterUser(r.Context(), &user)
+	if err != nil {
+		log.Error("Auth: ", err)
+		if errors.Is(err, errs.ErrBusyEmail) && errors.Is(err, errs.ErrBusyNickname) {
+			responses.DoBadResponse(w, http.StatusConflict, "Email and nickname are busy")
+		} else if errors.Is(err, errs.ErrBusyEmail) {
+			responses.DoBadResponse(w, http.StatusConflict, "Email is busy")
+		} else if errors.Is(err, errs.ErrBusyNickname) {
+			responses.DoBadResponse(w, http.StatusConflict, "Nickname is busy")
+		} else {
+			responses.DoBadResponse(w, http.StatusInternalServerError, "Internal server error")
+		}
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:     auth.SessionCookieName,
+		Value:    sessionID,
+		SameSite: http.SameSiteStrictMode,
+		Path:     "/",
+		HttpOnly: true,
+		MaxAge:   int((7 * 24 * time.Hour).Seconds()),
+	}
+	http.SetCookie(w, &cookie)
+
+	responses.DoEmptyOkResponse(w)
+}
+
+// LogoutUser разлогинивает пользователя
+func (d *UserDelivery) LogoutUser(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(auth.SessionCookieName)
+	if err != nil {
+		responses.DoBadResponse(w, http.StatusBadRequest, "You are not logged in")
+		return
+	}
+
+	sessionID := cookie.Value
+
+	err = d.userUC.LogoutUser(r.Context(), sessionID)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:   auth.SessionCookieName,
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	})
+
+	if err != nil {
+		responses.ResponseErrorAndLog(w, err, "LogoutUser")
+	} else {
+		responses.DoEmptyOkResponse(w)
+	}
+}
+
+// ChangePassword отвечает за смену пароля
+func (d *UserDelivery) ChangePassword(w http.ResponseWriter, r *http.Request) {
+	funcName := "ChangePassword"
+	userID, ok := requests.GetUserIDOrFail(w, r, funcName)
+	if !ok {
+		return
+	}
+	data := models.ChangePasswordRequest{}
+	err := requests.GetRequestData(r, &data)
+	if err != nil {
+		responses.DoBadResponse(w, http.StatusBadRequest, "bad request")
+		return
+	}
+	err = d.userUC.ChangePassword(r.Context(), userID, data.OldPassword, data.NewPassword)
+	if err != nil {
+		responses.DoBadResponse(w, http.StatusInternalServerError, "internal error")
+		return
+	}
+	responses.DoEmptyOkResponse(w)
 }
--- internal/pkg/user/interfaces.go
+++ internal/pkg/user/interfaces.go
@@ -11,10 +11,17 @@ type UserUsecase interface {
 	GetMyProfile(ctx context.Context, userID int) (profile *models.UserProfile, err error)
 	UpdateMyProfile(ctx context.Context, userID int, data *models.UserProfileUpdate) (updatedProfile *models.UserProfile, err error)
 	SetMyAvatar(ctx context.Context, userID int, file *multipart.File, fileHeader *multipart.FileHeader) (updated *models.UserProfile, err error)
+	LoginUser(ctx context.Context, email string, password string) (sessionID string, err error)
+	RegisterUser(ctx context.Context, user *models.UserRegistration) (sessionID string, err error)
+	LogoutUser(ctx context.Context, sessionID string) error
+	ChangePassword(ctx context.Context, userID int, oldPassword string, newPassword string) error
 }
 
 type UserRepo interface {
 	GetUserProfile(ctx context.Context, userID int) (profile *models.UserProfile, err error)
 	UpdateUserProfile(ctx context.Context, userID int, data models.UserProfileUpdate) (newProfile *models.UserProfile, err error)
 	SetUserAvatar(ctx context.Context, userID int, fileExtension string, fileSize int) (fileName string, err error)
+	GetUserByEmail(ctx context.Context, email string) (user *models.UserProfile, err error)
+	CreateUser(ctx context.Context, user *models.UserRegistration, hashedPassword string) (newUser *models.UserProfile, err error)
+	CheckUniqueCredentials(ctx context.Context, nickname string, email string) error
 }
