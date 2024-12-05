c550abaa9a1e2da6be810516fe61727ab4464a17
DeDxYk594 <zhugeo@mail.ru>
2024-12-05 18:24:24 +0300

    Мелкие исправления

--- internal/pkg/poll/usecase/user_uc.go
+++ internal/pkg/poll/usecase/user_uc.go
@@ -21,23 +21,25 @@ func CreatePollUsecase(pollRepo poll.PollRepo, authClient authGRPC.AuthClient) *
 }
 
 func (uc *PollUsecase) SubmitPoll(ctx context.Context, userID int64, pollSubmit *models.PollSubmit) error {
+	funcName := "SubmitPoll"
 	err := uc.pollRepo.SubmitPoll(ctx, userID, pollSubmit)
 	if err != nil {
-		return fmt.Errorf("SubmitPoll: %w", err)
+		return fmt.Errorf("%s: %w", funcName, err)
 	}
 
 	return nil
 }
 
 func (uc *PollUsecase) GetPollResults(ctx context.Context) (pollResults *models.PollResults, err error) {
+	funcName := "GetPollResults"
 	pollRating, err := uc.pollRepo.GetRatingResults(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("GetPollResults (GetRatingResults): %w", err)
+		return nil, fmt.Errorf("%s (GetRatingResults): %w", funcName, err)
 	}
 
 	pollText, err := uc.pollRepo.GetTextResults(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("GetPollResults (GetTextResults): %w", err)
+		return nil, fmt.Errorf("%s (GetTextResults): %w", funcName, err)
 	}
 
 	pollResults = &models.PollResults{
