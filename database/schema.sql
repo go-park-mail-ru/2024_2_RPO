4f8edaf97ed5fedfa54f4a8d2ac620790ca96d7c
DeDxYk594 <zhugeo@mail.ru>
2024-12-10 22:42:34 +0300

    Поправлено форматирование схемы БД

--- database/schema.sql
+++ database/schema.sql
@@ -1,23 +1,24 @@
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE user_uploaded_file(
-    file_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    file_uuid UUID NOT NULL DEFAULT uuid_generate_v4(),
-    file_hash TEXT,
+    file_id        BIGINT      PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    file_uuid      UUID        NOT NULL DEFAULT uuid_generate_v4(),
+    file_hash      TEXT,
     file_extension TEXT,
-    "size" INTEGER NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "size"         INTEGER     NOT NULL,
+    created_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "user" (
-    u_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nickname TEXT NOT NULL UNIQUE,
-    joined_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    password_hash TEXT,
-    csat_poll_dt TIMESTAMPTZ NOT NULL,
-    email TEXT UNIQUE NOT NULL,
+    u_id           BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nickname       TEXT        NOT NULL UNIQUE,
+    joined_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    password_hash  TEXT,
+    csat_poll_dt   TIMESTAMPTZ NOT NULL,
+    email          TEXT UNIQUE NOT NULL,
     avatar_file_id BIGINT,
+
     FOREIGN KEY (avatar_file_id) REFERENCES user_uploaded_file(file_id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
@@ -29,26 +30,27 @@ CREATE TYPE user_role AS ENUM (
 );
 
 CREATE TABLE board (
-    board_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_by BIGINT,
+    board_id            BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name"              TEXT        NOT NULL,
+    created_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by          BIGINT,
     background_image_id BIGINT,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     FOREIGN KEY (created_by) REFERENCES "user"(u_id) ON UPDATE CASCADE ON DELETE SET NULL,
     FOREIGN KEY (background_image_id) REFERENCES user_uploaded_file(file_id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE user_to_board (
-    u_id BIGINT NOT NULL,
-    board_id BIGINT NOT NULL,
-    added_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_visit_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    added_by BIGINT,
-    updated_by BIGINT,
+    u_id             BIGINT      NOT NULL,
+    board_id         BIGINT      NOT NULL,
+    added_at         TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_visit_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_by         BIGINT,
+    updated_by       BIGINT,
     invite_link_uuid UUID,
-    "role" user_role NOT NULL DEFAULT 'viewer',
+    "role"           user_role   NOT NULL DEFAULT 'viewer',
 
     FOREIGN KEY (u_id) REFERENCES "user"(u_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (board_id) REFERENCES board(board_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -58,25 +60,25 @@ CREATE TABLE user_to_board (
 );
 
 CREATE TABLE kanban_column (
-    col_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    board_id BIGINT NOT NULL,
-    title TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    order_index INT NOT NULL, -- Порядковый номер колонки на доске
+    col_id      BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    board_id    BIGINT      NOT NULL,
+    title       TEXT        NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    order_index INT         NOT NULL, -- Порядковый номер колонки на доске
 
     FOREIGN KEY (board_id) REFERENCES board(board_id) ON UPDATE CASCADE ON DELETE CASCADE,
     UNIQUE (board_id, order_index) DEFERRABLE INITIALLY DEFERRED
 );
 
 CREATE TABLE "card" (
-    card_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    card_uuid UUID NOT NULL DEFAULT uuid_generate_v4(), -- UUID для ссылки на карточку
-    title TEXT NOT NULL,
-    col_id BIGINT NOT NULL,
-    order_index INTEGER, -- Порядковый номер карточки в колонке
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    card_id       BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    card_uuid     UUID        NOT NULL DEFAULT uuid_generate_v4(), -- UUID для ссылки на карточку
+    title         TEXT        NOT NULL,
+    col_id        BIGINT      NOT NULL,
+    order_index   INTEGER, -- Порядковый номер карточки в колонке
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cover_file_id BIGINT,
     deadline TIMESTAMPTZ,
     is_done BOOLEAN NOT NULL DEFAULT FALSE, -- Видна, когда задан deadline или чеклист
@@ -87,12 +89,12 @@ CREATE TABLE "card" (
 
 CREATE TABLE card_attachment (
     attachment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    card_id BIGINT NOT NULL,
-    file_id BIGINT NOT NULL,
-    original_name TEXT NOT NULL,
-    attached_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    attached_by BIGINT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    card_id       BIGINT      NOT NULL,
+    file_id       BIGINT      NOT NULL,
+    original_name TEXT        NOT NULL,
+    attached_at   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    attached_by   BIGINT NOT  NULL,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (card_id) REFERENCES card(card_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (file_id) REFERENCES user_uploaded_file(file_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -100,24 +102,24 @@ CREATE TABLE card_attachment (
 );
 
 CREATE TABLE checklist_field (
-    checklist_field_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    card_id BIGINT NOT NULL,
-    title TEXT NOT NULL,
-    is_done BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    order_index INTEGER,
+    checklist_field_id BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    card_id            BIGINT      NOT NULL,
+    title              TEXT        NOT NULL,
+    is_done            BOOLEAN     NOT NULL DEFAULT FALSE,
+    created_at         TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    order_index        INTEGER,
 
     FOREIGN KEY (card_id) REFERENCES card(card_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE card_comment (
-    comment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    card_id BIGINT NOT NULL,
-    title TEXT NOT NULL,
-    created_by BIGINT NOT NULL,
+    comment_id BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    card_id    BIGINT      NOT NULL,
+    title      TEXT        NOT NULL,
+    created_by BIGINT      NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    is_edited BOOLEAN NOT NULL DEFAULT FALSE,
+    is_edited  BOOLEAN     NOT NULL DEFAULT FALSE,
 
     FOREIGN KEY (card_id) REFERENCES card(card_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (created_by) REFERENCES "user"(u_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -125,8 +127,8 @@ CREATE TABLE card_comment (
 
 CREATE TABLE card_user_assignment (
     assignment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    card_id BIGINT NOT NULL,
-    u_id BIGINT NOT NULL,
+    card_id       BIGINT NOT NULL,
+    u_id          BIGINT NOT NULL,
 
     FOREIGN KEY (card_id) REFERENCES card(card_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (u_id) REFERENCES "user"(u_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -138,20 +140,20 @@ CREATE TYPE question_type AS ENUM (
 );
 
 CREATE TABLE csat_question (
-    question_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    question_text TEXT NOT NULL,
-    "type" question_type NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    question_id   BIGINT        GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    is_active     BOOLEAN       NOT NULL DEFAULT TRUE,
+    question_text TEXT          NOT NULL,
+    "type"        question_type NOT NULL,
+    created_at    TIMESTAMPTZ   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE csat_results (
-    result_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    question_id BIGINT NOT NULL,
-    rating INTEGER,
-    comment TEXT,
-    u_id BIGINT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    result_id   BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    question_id BIGINT      NOT NULL,
+    rating      INTEGER,
+    comment     TEXT,
+    u_id        BIGINT      NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (u_id) REFERENCES "user"(u_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (question_id) REFERENCES csat_question(question_id) ON UPDATE CASCADE ON DELETE CASCADE
